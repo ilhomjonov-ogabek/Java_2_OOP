cdfef6c565a0a970b6edc1f3f4add43e144520b3
Ogabek <ilhomjonovo200@gmail.com>
2025-08-27 22:59:38 +0500

    Java_2

---
+++ com/pdp/online/task/polymorphism/animal/Animal.java
@@ -0,0 +1,12 @@
+package com.pdp.online.task.polymorphism.animal;
+
+public class Animal {
+  public void live(){
+    System.out.println("Living ");
+  }
+
+  public void eat(){
+    System.out.println("Eats");
+  }
+
+}
---
+++ com/pdp/online/task/polymorphism/texnika/Car.java
@@ -0,0 +1,8 @@
+package com.pdp.online.task.polymorphism.texnika;
+
+public class Car extends Texnika{
+  public void work(){
+    System.out.println("Car working");
+  }
+
+}
